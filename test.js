4173fded75f80de538a59faf9ba4981199ccbeb4
bmzhanglei <115097376@qq.com>
2021-06-13 22:58:11 +0800

    js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-let fs = require('fs')
-var zlib = require('zlib')
-const url = require("url")
-const http=require("http")
-const https=require("https")
-const querystring = require('querystring')
+// let fs = require('fs')
+// var zlib = require('zlib')
+// const url = require("url")
+// const http=require("http")
+// const https=require("https")
+// const querystring = require('querystring')
 
-const {event} = require("./event.js")
-const { on } = require('events')
+// const {event} = require("./event.js")
+// const { on } = require('events')
 // fs.createReadStream('./package-lock.json').pipe(zlib.createGzip()).pipe(fs.createWriteStream('test1.zip'));
 
 // const val = process.env.npm_package_config_author
@@ -18,7 +18,7 @@ const { on } = require('events')
 // // console.log(url.resolve(www,"../") )
 
 // const server = http.createServer((request,response)=>{
- 
+
 // })
 
 // const postData = querystring.stringify([{},{"catId":"5fe309e096ee9288ed504dd1"}])
@@ -60,87 +60,145 @@ const { on } = require('events')
 // event.emit('play', '我和我的祖国')
 
 
-const arr = [
-    {name:"1-",id:1,pid:"0"},
-    {name:"11-",id:11,pid:"1"},
-    {name:"111-",id:111,pid:"11"},
-    {name:"111xx-",id:1110,pid:"111"},
-    {name:"1111-",id:1111,pid:"111"},
-    {name:"11112-",id:1112,pid:"1111"},
-    {name:"2-",id:2,pid:""},
-    {name:"22-",id:22,pid:"2"},
-    {name:"22xx",id:222,pid:"22"},
-    {name:"22xxy",id:223,pid:"22"},
+const arr = [{
+        name: "1-",
+        id: 1,
+        pid: "0"
+    },
+    {
+        name: "11-",
+        id: 11,
+        pid: "1"
+    },
+    {
+        name: "111-",
+        id: 111,
+        pid: "11"
+    },
+    {
+        name: "2-",
+        id: 2,
+        pid: ""
+    },
+    {
+        name: "111xx-",
+        id: 1110,
+        pid: "111"
+    },
+    {
+        name: "1111-",
+        id: 1111,
+        pid: "111"
+    },
+    {
+        name: "11112-",
+        id: 1112,
+        pid: "1111"
+    },
+    {
+        name: "22-",
+        id: 22,
+        pid: "2"
+    },
+    {
+        name: "22xx",
+        id: 222,
+        pid: "22"
+    },
+    {
+        name: "22xxy",
+        id: 223,
+        pid: "22"
+    },
 ]
 
 
-
 let obj = {}
-arr.forEach(item=>{     
-    obj[item.id] = item
-    if(obj.pid === ""){
+arr.forEach(item => {
+    obj[item.id + "---" + (item.pid ? item.pid : "0")] = item
+    if (obj.pid === "") {
         obj[item.id].pid = "0"
-    }  
+    }
 })
 
-function getParentData(ids=[],item){
-    let {_,pid} = item
-    const val =  obj[pid]
-    if(val && pid){        
-        ids.unshift(pid)      
-        getParentData(ids,val)
-    }       
+
+
+
+
+function getParentData(ids = [], item) {
+    let {
+        _,
+        pid
+    } = item
+    const val = obj[pid]
+    if (val && pid) {
+        ids.unshift(pid)
+        getParentData(ids, val)
+    }
     return ids
 }
 
 //arr 添加 pids
-arr.forEach(item=>{
-    if(item.pid === ""){
+arr.forEach(item => {
+    if (item.pid === "") {
         item.pid = "0"
-    }  
-    item.pids = getParentData([],item)       
+    }
+    item.pids = getParentData([], item)
 })
 
 //objs 添加 pids
-for(let i in obj){
-    obj[i].pids = getParentData([],obj[i])     
+for (let i in obj) {
+    obj[i].pids = getParentData([], obj[i])
 }
 
+console.log(obj)
 
-function getChild(pids,index){
-    if(pids.length){
-       let one = trees[index].filter(res=>res.id===pids[0]) 
-
-       return  getChilds(pids,one)
-    //    if(pids[1]){
-    //       const two = one.children.filter(res=>res.id===pids[1])
-    //        if(pids[2]){
-    //           const three = two.children.filter(res=>res.id===pids[2])
-    //        }else{
-    //            return two.children
-    //        }
-    //    }else{
-    //        return one.children
-    //    }
-
-    }else{
-       return trees[index].children
-    }
-}
-
-
-function getChilds(pids,one){
-    let child = one.children
-    for(let i=1;i<pids.length;i++){       
-        if(pids[i]){
-           one = child.filter(res=>res.id===pids[i])
-        }else{
-            return one.children
+function arrayToTree(arr, pid) {
+    let temp = [];
+    let treeArr = arr;
+    treeArr.forEach((item, index) => {
+        if (item.pid == pid) {
+            const treedata = arrayToTree(treeArr, treeArr[index].id)
+            if (treedata.length > 0) {
+                treeArr[index].children = treedata;
+            }
+            temp.push(treeArr[index]);
         }
-    }
+    });
+    return temp;
 }
 
+console.log(JSON.stringify(arrayToTree(arr, "0"), null, 2));
+
+// function getChild(pids,index){
+//     if(pids.length){
+//        let one = trees[index].filter(res=>res.id===pids[0]) 
+
+//        return  getChilds(pids,one)
+//     //    if(pids[1]){
+//     //       const two = one.children.filter(res=>res.id===pids[1])
+//     //        if(pids[2]){
+//     //           const three = two.children.filter(res=>res.id===pids[2])
+//     //        }else{
+//     //            return two.children
+//     //        }
+//     //    }else{
+//     //        return one.children
+//     //    }
+
+//     }else{
+//        return trees[index].children
+//     }
+// }
 
 
-trees.filter(res=>res.pids.length)
-
+// function getChilds(pids,one){
+//     let child = one.children
+//     for(let i=1;i<pids.length;i++){       
+//         if(pids[i]){
+//            one = child.filter(res=>res.id===pids[i])
+//         }else{
+//             return one.children
+//         }
+//     }
+// }
